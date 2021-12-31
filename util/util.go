a0bb6afd477091736861c91f4fd01169e4acf374
spinales <saul021002@gmail.com>
2021-12-31 02:11:33 -0400

    adding return methods to return JSON on requests

--- util/util.go
+++ util/util.go
@@ -1,6 +1,11 @@
 package util
 
-import "math/rand"
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+)
 
 var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
@@ -10,4 +15,18 @@ func RandSeq(n int) string {
 		b[i] = letters[rand.Intn(len(letters))]
 	}
 	return string(b)
+}
+
+// respondwithError return error message
+func RespondWithError(w http.ResponseWriter, code int, msg string) {
+	RespondwithJSON(w, code, map[string]string{"message": msg})
+}
+
+// respondwithJSON write json response format
+func RespondwithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	fmt.Println(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
 }
