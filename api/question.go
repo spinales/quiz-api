9e0e6fa1206007cff6888d8e79e751f8e018abe2
spinales <saul021002@gmail.com>
2022-01-17 16:03:57 -0400

    improve api/question.go file

--- api/question.go
+++ api/question.go
@@ -10,6 +10,9 @@ import (
 	"github.com/spinales/quiz-api/util"
 )
 
+// AddQuestion endpoint para agregar una pregunta nueva, el front envia
+// un json con la estructura de la preguta, la pregunta en si, la url de
+// una imagen y un array de strings que son las respuestas.
 func (s *Server) AddQuestion(w http.ResponseWriter, r *http.Request) {
 	var q models.Question
 	json.NewDecoder(r.Body).Decode(&q)
@@ -22,6 +25,8 @@ func (s *Server) AddQuestion(w http.ResponseWriter, r *http.Request) {
 	util.RespondwithJSON(w, http.StatusOK, q)
 }
 
+// UpdateQuestion endpoint para actualizar una pregunta, similar a
+// AddQuestion pero en la url se agrega el id de la pregunta aactualizar.
 func (s *Server) UpdateQuestion(w http.ResponseWriter, r *http.Request) {
 	var req models.Question
 	json.NewDecoder(r.Body).Decode(&req)
@@ -39,6 +44,8 @@ func (s *Server) UpdateQuestion(w http.ResponseWriter, r *http.Request) {
 	util.RespondwithJSON(w, http.StatusOK, q)
 }
 
+// DeleteQuestion endpoint para borrar una pregunta, se recibe el id
+// y se elimina el la pregunta que corresponda al id mandado.
 func (s *Server) DeleteQuestion(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.ParseUint(chi.URLParam(r, "id"), 10, 64)
 	if err != nil {
@@ -53,6 +60,7 @@ func (s *Server) DeleteQuestion(w http.ResponseWriter, r *http.Request) {
 	util.RespondwithJSON(w, http.StatusOK, nil)
 }
 
+// GetQuestion endpoint para retornar la pregunta en base al id recibido
 func (s *Server) GetQuestion(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.ParseUint(chi.URLParam(r, "id"), 10, 64)
 	if err != nil {
@@ -67,6 +75,7 @@ func (s *Server) GetQuestion(w http.ResponseWriter, r *http.Request) {
 	util.RespondwithJSON(w, http.StatusOK, q)
 }
 
+// GetQuestions endpoint para retornar todas las preguntas existentes
 func (s *Server) GetQuestions(w http.ResponseWriter, r *http.Request) {
 	qs, err := s.service.QuestionService.GetQuestions()
 	if err != nil {
